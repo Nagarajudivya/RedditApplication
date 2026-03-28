1fac8c2c9add4cd6d533a26436edb9dbc14f7bcc
Divya Nagaraju <nagaraju.divya.36.2@mountblue.tech>
2026-03-28 18:59:23 +0530

    Initial commit - Reddit

---
+++ src/main/java/com/RedditApplication/demoReddit/DemoRedditApplication.java
@@ -0,0 +1,16 @@
+package com.RedditApplication.demoReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRedditApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoRedditApplication.class, args);
+
+		System.out.println("hello");
+	}
+
+}
